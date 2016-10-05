4eb8634a7caaee0c4bd99b9a2024524f02a364e3
Vignesh <emvignesh16@gmail.com>
2016-10-05 21:21:21 +0530

    Students' name updated in the list in activity_details

--- app/src/main/java/edu/mitinda/logger/DetailsActivityFragment.java
+++ app/src/main/java/edu/mitinda/logger/DetailsActivityFragment.java
@@ -32,6 +32,8 @@ public class DetailsActivityFragment extends Fragment {
     ArrayAdapter<String> mStudentsAdapter;
     ListView listView;
     String params[];
+    String course;
+    String tregNo;
     String teacherCourseId = null;
 
     public DetailsActivityFragment() {
@@ -40,6 +42,18 @@ public class DetailsActivityFragment extends Fragment {
     @Override
     public void onStart() {
         super.onStart();
+        updateStudentNameListView();
+    }
+
+    private void updateStudentNameListView() {
+        FetchStudentsTask studentsName = new FetchStudentsTask();
+        Intent intent = getActivity().getIntent();
+        tregNo = intent.getStringExtra("tregNo");
+        course = intent.getStringExtra("course");
+        studentsName.execute(new String[] {
+                tregNo,
+                course
+        });
     }
 
     @Override
@@ -57,10 +71,110 @@ public class DetailsActivityFragment extends Fragment {
         ArrayList<String> students = new ArrayList<String>(Arrays.asList(data));
 
         mStudentsAdapter = new ArrayAdapter<String>(getActivity(), R.layout.details_listitem_textview,
-                R.id.details_textview, students);
+                R.id.details_textview, new ArrayList<String>());
         listView = (ListView) view.findViewById(R.id.details_listview);
         listView.setAdapter(mStudentsAdapter);
 
         return view;
+    }
+
+
+    public class FetchStudentsTask extends AsyncTask<String[], Void, String[]> {
+
+        private String[] getStudentsFromJson(String studentJsonString) {
+            String course[] = null;
+            try {
+                JSONArray jsonArray = new JSONArray(studentJsonString);
+                JSONObject jsonObject;
+                int length = jsonArray.length();
+                course = new String[length];
+
+                for(int i = 0 ; i < length; i++) {
+                    jsonObject = jsonArray.getJSONObject(i);
+                    course[i] = jsonObject.optString("name").toString();
+                }
+
+            } catch (JSONException e) {
+                e.printStackTrace();
+            }
+            return course;
+        }
+
+        @Override
+        protected String[] doInBackground(String[]... params) {
+            if(params.length == 0) {
+                return null;
+            }
+
+            HttpURLConnection urlConnection = null;
+            URL url = null;
+            String studentJsonString = null;
+            BufferedReader reader = null;
+
+            try {
+                url = new URL("http://vmobilebase.hol.es/tracker/select-students.php?teacherCourseId=" + params[0]);
+                urlConnection = (HttpURLConnection) url.openConnection();
+                urlConnection.connect();
+
+                InputStream inputStream = urlConnection.getInputStream();
+                StringBuffer buffer = new StringBuffer();
+
+                if(inputStream == null) {
+                    return null;
+                }
+
+                reader = new BufferedReader(new InputStreamReader(inputStream));
+                String line;
+
+                while((line = reader.readLine()) != null) {
+                    buffer.append(line + "\n");
+                }
+
+                if(buffer.length() == 0) {
+                    return null;
+                }
+
+                studentJsonString = buffer.toString();
+
+            } catch (MalformedURLException e) {
+                e.printStackTrace();
+            } catch (IOException e) {
+                e.printStackTrace();
+            } finally {
+                if(urlConnection != null) {
+                    urlConnection.disconnect();
+                }
+
+                if(reader != null) {
+                    try {
+                        reader.close();
+                    } catch (Exception e) {
+
+                    }
+                }
+            }
+
+            try {
+                return getStudentsFromJson(studentJsonString);
+            }
+            catch (Exception e) {
+
+            }
+
+            return null;
+        }
+
+        @Override
+        protected void onPostExecute(String[] strings) {
+            super.onPostExecute(strings);
+            if(strings != null) {
+                mStudentsAdapter.clear();
+                int i = 0;
+                for(String name : strings) {
+                    mStudentsAdapter.add(name);
+                    Log.v("Helloooooooo", mStudentsAdapter.getItem(i++));
+                }
+            }
+        }
     }
 }
